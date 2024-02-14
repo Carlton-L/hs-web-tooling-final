4813d520069515fcf1ba2f862bdcac91d1b9f835
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2024-02-14 20:41:08 +0100

    Fixed deployment errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
   plugins: [react()],
+  base: '/hs-web-toolng-final/',
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./tests/setup",
+    environment: 'jsdom',
+    setupFiles: './tests/setup',
   },
   server: {
     proxy: {
-      "/generate_insult.php?lang=en&type=json": {
-        target: "https://evilinsult.com/generate_insult.php?lang=en&type=json", // The URL of the external API
+      '/generate_insult.php?lang=en&type=json': {
+        target: 'https://evilinsult.com/generate_insult.php?lang=en&type=json', // The URL of the external API
         changeOrigin: true, // Needed for virtual hosted sites
-        rewrite: (path) => path.replace(/^\/api/, ""), // Remove /api from the path
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api from the path
       },
     },
   },
